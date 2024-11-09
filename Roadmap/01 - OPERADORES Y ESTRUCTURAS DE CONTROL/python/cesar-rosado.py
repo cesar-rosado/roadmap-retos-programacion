49e5e747df2bda5ca4b17c04e490d4a55467d1ae
César-Rosado-PC <cirv84@hotmail.com>
2024-11-08 19:23:24 -0500

    Agregando operadores ariméticos, lógicos y de comparación

---
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/cesar-rosado.py
@@ -0,0 +1,35 @@
+# 01 OPERADORES Y ESTRUCTURAS DE CONTROL
+
+# EJERCIO 1: TIPOS DE OPERADORES
+
+# Operadores Aritméticos
+print("")
+print("*** Operadores Aritméticos ***")
+print(f"La suma de 5 + 10 es: {5 + 10}")
+print(f"La resta de 10 - 5 es: {10 - 5}")
+print(f"La multiplicación de 3 * 5 es: {3 * 5}")
+print(f"La división de 10 / 2 es: {10 / 2}")
+print(f"La división entera de 10 // 3 es: {10 // 3}")
+print(f"El módulo 14 % 4 es: {14 % 4}")
+print(f"2 elevado al cubo es: {2 ** 3}")
+print("")
+
+# Operadores Lógicos
+print("*** Operadores Lógicos ***")
+operador_and = 5 > 6 and 2 < 10
+operador_or = 10 > 12 or 2 + 2 == 4
+operador_not = not ("a" == "b")
+print(f"Operador AND: {operador_and}")
+print(f"Operador OR: {operador_or}")
+print(f"Operador NOT: {operador_not}")
+print("")
+
+# Operadores de comparación
+print("*** Operadores de comparación ***")
+print(f"100 es igual a 100:     {100 == 100}")
+print(f"5 es diferente a 10:    {5 != 10} ")
+print(f"3 es mayor 6:           {3 > 6}")
+print(f"3 es mayor o igual 2:   {3 >= 2}")
+print(f"3 es menor 6:           {3 < 6}")
+print(f"3 es menor o igual 2:   {3 <= 2}")
+print("")
