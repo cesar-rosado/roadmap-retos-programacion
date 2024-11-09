3e97127a722937846bf0d952d12545fc7b474fa8
César Rosado <cirv84@hotmail.com>
2024-11-09 16:13:02 -0600

    Agregando operadores de asignación, identidad, pertenencia y de bits

--- Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/cesar-rosado.py
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/cesar-rosado.py
@@ -1,7 +1,7 @@
 # 01 OPERADORES Y ESTRUCTURAS DE CONTROL
 
 # EJERCIO 1: TIPOS DE OPERADORES
-
+print("#### EJERCIO 1: TIPOS DE OPERADORES ####")
 # Operadores Aritméticos
 print("")
 print("*** Operadores Aritméticos ***")
@@ -32,4 +32,67 @@ print(f"3 es mayor 6:           {3 > 6}")
 print(f"3 es mayor o igual 2:   {3 >= 2}")
 print(f"3 es menor 6:           {3 < 6}")
 print(f"3 es menor o igual 2:   {3 <= 2}")
+print("")
+
+# Operadores de asignación
+print("*** Operadores de asignación ***")
+num = 5
+print(f"Variable num = {num}")
+print(f"Operador '=': asigna un valor a una varible 'x = {num}' ")
+num += 1
+print(f"Operador '+=': suma el valor asignado (1) a la variable num' {num}'")
+num = 5
+num -= 1
+print(f"Operador '-=': resta el valor asignado (1) a la variable num '{num}' ")
+num = 5
+num *= 2
+print(f"Operador '*=': multiplica el valor asignado (2) a la variable num '{num}'")
+num = 10
+print(f"Ahora num = {num}")
+num /= 2
+print(f"Operador '/=': divide el valor asignado (2) a la variable num '{num}'")
+num = 10
+num %= 2
+print(f"Operador '%=': Aplica el módulo asignado (2) a la variable num '{num}'")
+num = 10
+num //= 2
+print(
+    f"Operador '//=': Aplica la división entera asignada (2) a la variable num '{num}' "
+)
+num = 10
+num **= 2
+print(f"Operador '**=': Potencia la variable num al valor asignado (2) '{num}'")
+print("")
+
+# Operadores de identidad
+print("*** Operadores de identidad ***")
+a = [9, 11, 2]
+b = a
+c = [2, 9, 11]
+print(f"a = {a}\nb = {b}\nc = {c}")
+print(f"a is b = {(a is b)}")
+print(f"a is c = {(a is c)}")
+print("")
+
+# Operadores de pertenencia
+print("*** Operadores de pertenencia ***")
+print(f"11 se encuentra en la lista 'a': {(11 in a)}")
+print(f"7 se encuentra en 'a': {(7 in a)}")
+print(f"20 no se encuentra en 'a': {(20 not in a)}")
+print("")
+
+# Operadores de bits
+a = 5
+b = 8
+print(f"a = {a}\nb = {b}")
+print(f"AND: {a & b}")
+print(f"OR: {a | b}")
+print(f"XOR: {a ^ b}")
+print(f"NOT: {~a}")
+print(f"Desplazamiento a la izquierda: a << 1 = {a << 1}")
+print(f"Desplazamiento a la derecha: a >> 1 = {a >> 1}")
+print("")
+
+# EJERCICIO 2: TIPOS DE ESTRUCTURA DE CONTROL
+print("### EJERCICIO 2: TIPOS DE ESTRUCTURA DE CONTROL ####")
 print("")
